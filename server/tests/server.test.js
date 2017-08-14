e7972f178008d024258a24cc19b9a35e4c65591c
Joel Klaus <jaklaus0101@gmail.com>
2017-08-14 15:25:50 -0500

    Test GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First Test Todo'
+},
+{
+  text: 'Second Test Todo'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 })
 
 describe('POST /todos', () => {
@@ -24,7 +33,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -45,9 +54,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done());
       })
   });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done)
+  })
 });
