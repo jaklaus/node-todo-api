10cf5ddc51357dc7e957740e3f1fc4040b315d9c
Joel Klaus <jaklaus0101@gmail.com>
2017-08-18 14:03:43 -0500

    Create tests for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.delete('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send('Todo not found')
     }
-    res.status(200).send(todo)
+    res.status(200).send({todo})
   }).catch((e) => {
     res.status(400).send();
   });
