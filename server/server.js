fb705e280dfca234c4a2e5a1a8e9f4c89e99205b
Joel Klaus <jaklaus0101@gmail.com>
2017-08-18 17:12:51 -0500

    Changed GET /todos route back to original to fix bug in test file

--- server/server.js
+++ server/server.js
@@ -29,8 +29,8 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todo) => {
-    res.send({todo})
+  Todo.find().then((todos) => {
+    res.send({todos})
   }, (e) => {
     res.status(400).send(e);
   });
