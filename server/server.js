394a8ca3326aaae4b573a432f35cfbfc44f8cd3f
Joel Klaus <jaklaus0101@gmail.com>
2017-08-18 16:04:47 -0500

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -63,6 +64,34 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('Object id invalid');
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = _.now()
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send('Todo does not exist.');
+    }
+
+    res.send({todo});
+
+  }).catch((e) => {
+    console.log(e)
+    res.status(400).send(`Could not update ${{id}}`)
+})
 
 });
 
